136d73fcecfb1af5002e4e43e61d7fe948883e28
Luisao <luisaodeben@gmail.com>
2025-09-09 19:59:47 +0200

    New dependences: mysql2 and zod

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-export class user {
+export class userController {
   static getAll = (req, res) => {
     res.send('Mostrar todos')
   }
@@ -6,5 +6,11 @@ export class user {
   static getById = (req, res) => {
     const { id } = req.params
     res.send(`La id del usuario es ${id}`)
+  }
+
+  static create = (req, res) => {
+    const { name } = req.body
+    res.status(201)
+    res.send(`usuario creado con el nombre ${name}`)
   }
 }
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { user } from '../controllers/user.js'
+import { userController as user } from '../controllers/userController.js'
 
 export const routerUsers = express.Router()
 
 routerUsers.get('/', user.getAll)
 routerUsers.get('/:id', user.getById)
+routerUsers.post('/', user.create)
